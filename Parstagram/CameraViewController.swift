b8a476633b3e24b41f1a6b7f972ab16de698c376
Tiffany <tiff.lee016@gmail.com>
2020-11-16 13:09:10 -0800

    Initial Commit

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,83 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Tiffany Lee on 11/10/20.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.isUserInteractionEnabled = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject (className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()
+        
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print ("error!")
+            }
+        }
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        view.endEditing(true)
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        imageView.isUserInteractionEnabled = true
+        var tapGesture = UITapGestureRecognizer(target: self, action: Selector("imagePickerController"))
+           self.view.addGestureRecognizer(tapGesture)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.isUserInteractionEnabled = true
+        let image = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to:size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
